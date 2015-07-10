7b92cfc30bbb8237dbbf83346a6001e0bd59c5b9
missylbytes <melisagriff@gmail.com>
2015-07-09 21:41:21 -0400

    added geolocation to auto fill in form

--- sellerSignUp2.php
+++ sellerSignUp2.php
@@ -1,5 +1,102 @@
 <?php include 'verify2.php'?>
+ 
+ 
+ 
+ <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script> 
+<script type="text/javascript"> 
+  var geocoder;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+} 
+//Get the latitude and the longitude;
+function successFunction(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    codeLatLng(lat, lng)
+}
 
+function errorFunction(){
+    alert("Geocoder failed");
+}
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+}
+  
+function codeLatLng(lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    var streetFound = 0;
+    var cityFound = 0;
+    var stateFound = 0;
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+      console.log(results)
+        if (results[1]) {
+          
+          //find city name
+           for (var i=0; i<results[0].address_components.length; i++) {
+            for (var b=0;b<results[0].address_components[i].types.length;b++) {
+            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                    //this is the object you are looking for
+                    state = results[0].address_components[i];
+                    break;
+                }
+                   
+                }
+            }
+            for (var i=0; i<results[0].address_components.length; i++) {
+               
+              for (var b=0;b<results[0].address_components[i].types.length;b++) {
+              //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                  if(results[0].address_components[i].types[b] == "locality"){
+                      city = results[0].address_components[i];
+                      break;
+                   }
+               }
+                   
+            }
+            for (var i=0; i<results[0].address_components.length; i++) {
+               
+              for (var b=0;b<results[0].address_components[i].types.length;b++) {
+              //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                  if(results[0].address_components[i].types[b] == "street_number"){
+                      street_num = results[0].address_components[i];
+                      break;
+                   }
+               }
+                   
+            }
+            for (var i=0; i<results[0].address_components.length; i++) {
+               
+              for (var b=0;b<results[0].address_components[i].types.length;b++) {
+              //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                  if(results[0].address_components[i].types[b] == "route"){
+                      street = results[0].address_components[i];
+                      break;
+                   }
+               }
+                   
+            }
+            //Fills the document
+            document.getElementById('street').value= street_num.long_name + " " + street.long_name; 
+            document.getElementById('city').value = city.long_name; 
+            document.getElementById('state').value = state.long_name;
+        }
+        else {
+          alert("No results found");
+        }
+      }
+      else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+    
+    
+  }
+        
+</script>
+<body onload = "initialize(); populate();">
 <h2 class="signupSeller">Seller Registration!</h2>
 
 <div id="mainContainer">
@@ -18,19 +115,19 @@
                 <div class="form-group">
                   <div class="right-inner-addon">
                     <i class="glyphicon glyphicon-map-marker"></i>
-                    <input class="form-control input-lg" placeholder="Street Address" type="text" name="street">
+                    <input id = "street" class="form-control input-lg" placeholder="Street Address" type="text" name="street">
                   </div>
                 </div>
                 <div class="form-group">
                   <div class="right-inner-addon">
                     <i class="glyphicon glyphicon-map-marker"></i>
-                    <input class="form-control input-lg" placeholder="City" type="text" name="city">
+                    <input id = "city" class="form-control input-lg" placeholder="City" type="text" name="city">
                   </div>
                 </div>
                 <div class="form-group">
                   <div class="right-inner-addon">
                     <i class="glyphicon glyphicon-map-marker"></i>
-                    <input class="form-control input-lg" placeholder="State" type="text" name="state">
+                    <input id = "state" class="form-control input-lg" placeholder="State" type="text" name="state">
                   </div>
                 </div>          
                 <input type="submit" class="btn btn-primary" value="Continue">
@@ -43,6 +140,8 @@
 </div>		
 </div>
 </div>
+</body>
 <?php include 'header.php'?>
 <?php include 'footer.php' ?>
+
  
