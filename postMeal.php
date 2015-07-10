4fd61507edd984dd36c2e7164438c9d13439b881
esaith <hounds2003@hotmail.com>
2015-07-10 06:12:44 -0400

    slight change to upload. Upload of picture works along with basic data to the server.

--- postMeal.php
+++ postMeal.php
@@ -15,7 +15,7 @@ if($_POST)
     
     if ($connection->select_db("avengers_DB_mouse") === false)
       echo "Could not select requested database";  
-            
+    
     //$insertStmt = sprintf("INSERT INTO `avengers_db_mouse`.`available_recipes` (`id`, `username`, `password`, `street`, `city`, `state`, `phone`, `email`) 
     //          VALUES (NULL, '%s', '%s', '', '', '', '', '%s')",  
     //          $connection->real_escape_string($_POST["username"]),
@@ -36,22 +36,78 @@ if($_POST)
     else
       echo "Problem with connection query.";        
 
-    $host= $_SERVER["HTTP_HOST"];
+
+
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) 
+    {
+      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      if($check !== false) 
+      {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+      } 
+      else 
+      {
+        echo "File is not an image.";
+        $uploadOk = 0;
+      }
+    }
+    // Check if file already exists
+    if (file_exists($target_file)) 
+    {
+      echo "Sorry, file already exists.";
+      $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) 
+    {
+      echo "Sorry, your file is too large.";
+      $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) 
+    {
+      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) 
+    {
+      echo "Sorry, your file was not uploaded.";
+      // if everything is ok, try to upload file
+    } 
+    else 
+    {
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+      {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+      } else 
+      {
+        echo "Sorry, there was an error uploading your file.";
+      }
+      
+      
+      $host= $_SERVER["HTTP_HOST"];
       $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
       header("Location: http://$host$path/index.php");
       $connection->close();  
       exit;
-  }
- 
-  
-  
+    }    
+  }  
 }
 
 ?>
 
 <h2 class="signupSeller">Post a meal for sale.</h2>
 
-<form action="upload.php" method="POST" enctype="multipart/form-data">
+<form action="<? $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">
   <div class="container">
     <div class="row">
       <div class="col-md-4 col-md-offset-4">
